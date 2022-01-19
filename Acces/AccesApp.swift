a6ff857cde4591bb3a34e27de8e00c1f390a380c
josh <josch>
2022-01-19 15:39:03 +0100

    Initial Commit

---
+++ Acces/AccesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccesApp.swift
+//  Acces
+//
+//  Created by Slawomir on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AccesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
